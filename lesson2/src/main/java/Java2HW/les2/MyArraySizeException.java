1a77aae23a572b43e1202b65c9b8339566a57511
Максим Харитонов <96545249+MaxKharitonov@users.noreply.github.com>
2022-03-27 16:49:27 +0300

    Update MyArraySizeException.java
    Убрана проверка размера массива, изменено наследование класса.

--- lesson2/src/main/java/Java2HW/les2/MyArraySizeException.java
+++ lesson2/src/main/java/Java2HW/les2/MyArraySizeException.java
@@ -1,13 +1,10 @@
 package Java2HW.les2;
 
-import java.io.IOException;
-
-public class MyArraySizeException {
+public class MyArraySizeException extends RuntimeException {
     int row, column;
 
-    public MyArraySizeException(int row, int column) throws IOException {
-        if (row != 4 || column != 4)
-            throw new IOException("Исключение: Значения строк и столбцов могут быть равны только 4." + "\nВведенные значения: строк: " + row + ", столбцов: " + column + "\nПродолжаю работу: Задан размер массива: строк: " + row + ", столбцов." + column);
+    public MyArraySizeException(int row, int column) {
+        super("Ошибка: Значения строк и столбцов могут быть равны только 4." + "\nВведенные значения: строк: " + row + ", столбцов: " + column);
         this.row = row;
         this.column = column;
     }
